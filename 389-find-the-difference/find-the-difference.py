68172bc3aab0bda1e7d1155d0b293e9ee84b3dd0
Lalesh Kumar Raj <laleshkumarraj2005@gmail.com>
2026-07-10 18:51:14 +0530

    Time: 3 ms (63.29%) | Memory: 12.5 MB (29.17%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        freq = {}
+        for ch in s:
+            if ch in freq:
+                freq[ch]+=1
+            else:
+                freq[ch]=1
+        for ch in t:
+            if ch in freq and freq[ch]!=0:
+                freq[ch]-=1
+            else:
+                return ch
