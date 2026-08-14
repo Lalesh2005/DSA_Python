2ee259b8a3c0a4a8f1ca68598fd3b30139e8b9c0
Lalesh Kumar Raj <laleshkumarraj2005@gmail.com>
2026-08-14 18:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (60.12%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,34 +11,57 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int count=0;
-        ListNode* temp=NULL;
-        temp=head;
-        while(temp!=NULL)
-        {
-            count++;
-            temp=temp->next;
-        }
-        if(count==n)
-        {
-            head=head->next;
-            return head;
-        }
-        ListNode* curr=NULL;
-        ListNode* prev=NULL;
-        curr=head;
-        for(int i=0;i<(count-n);i++)
-        {
-            prev=curr;
-            curr=curr->next;
-        }
-        if(curr->next==NULL)
-        {
-            prev->next=NULL;
-            return head;
-        }
-        prev->next=curr->next;
-        return head;
+    //     int count=0;
+    //     ListNode* temp=NULL;
+    //     temp=head;
+    //     while(temp!=NULL)
+    //     {
+    //         count++;
+    //         temp=temp->next;
+    //     }
+    //     if(count==n)
+    //     {
+    //         head=head->next;
+    //         return head;
+    //     }
+    //     ListNode* curr=NULL;
+    //     ListNode* prev=NULL;
+    //     curr=head;
+    //     for(int i=0;i<(count-n);i++)
+    //     {
+    //         prev=curr;
+    //         curr=curr->next;
+    //     }
+    //     if(curr->next==NULL)
+    //     {
+    //         prev->next=NULL;
+    //         return head;
+    //     }
+    //     prev->next=curr->next;
+    //     return head;
+
+
+//_____OPTIMIZED method
+    ListNode* dummy = new ListNode(0);
+dummy->next = head;
+
+ListNode* slow = dummy;
+ListNode* fast = dummy;
+
+for(int i=0;i<=n;i++)
+{
+    fast = fast->next;
+}
+
+while(fast)
+{
+    slow = slow->next;
+    fast = fast->next;
+}
+
+slow->next = slow->next->next;
+
+return dummy->next;
         
     }
 };
